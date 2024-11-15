77e21e3fe5e4d89aef98b623ae42c561c8631bef
iryna-mertsalova <iryna.mertsalova@nerdysoft.com>
2024-11-15 18:00:10 +0200

    fix: update downloading icons and colors to card-item

--- src/app/ui-kit/constants/card-item.ts
+++ src/app/ui-kit/constants/card-item.ts
@@ -1,9 +1,9 @@
 export type CardItemStyle = { icon: string; color: string };
 
 export const cardItemStyles: CardItemStyle[] = [
-  { icon: '/avatars/Profile.svg', color: 'shade' },
-  { icon: '/avatars/Profile-1.svg', color: 'success' },
-  { icon: '/avatars/Profile-2.svg', color: 'error' },
-  { icon: '/avatars/Profile-3.svg', color: 'secondary' },
-  { icon: '/avatars/Profile.svg', color: 'accent' },
+  { icon: '/avatars/Profile.svg', color: 'bg-shade' },
+  { icon: '/avatars/Profile-1.svg', color: 'bg-success' },
+  { icon: '/avatars/Profile-2.svg', color: 'bg-error' },
+  { icon: '/avatars/Profile-3.svg', color: 'bg-secondary' },
+  { icon: '/avatars/Profile-1.svg', color: 'bg-accent' },
 ];
