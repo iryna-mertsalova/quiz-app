6e75af1938bf7b59d8b5647d0a07f0a8263d0e52
iryna-mertsalova <iryna-mertsalova@users.noreply.github.com>
2024-11-19 17:38:21 +0000

    Deploying to prod from @ iryna-mertsalova/quiz-app@fd989fe9d4e39817db0a4fdc75ee6413cb3b0ffd 🚀

---
+++ chunk-LPU4FWXT.js
@@ -0,0 +1,2 @@
+import{K as m,M as s,N as r,g as a,m as t,n,u as i,w as p}from"./chunk-J5XQNO5I.js";var u=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=a({type:o,selectors:[["app-quiz"]],standalone:!0,features:[p],decls:5,vars:0,consts:[[1,"text-bright"],["link","/main"],["size","sm","type","ghost","iconState","prev"]],template:function(e,l){e&1&&(t(0,"p",0),i(1,` Quiz mocked page
+`),n(),t(2,"app-ui-link",1)(3,"app-ui-button",2),i(4,"Go to main page"),n()())},dependencies:[m,s,r],encapsulation:2})};export{u as QuizComponent};
