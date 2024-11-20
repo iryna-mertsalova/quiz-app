43d61642ca8027cd8cf6334637757a6a3138805b
iryna-mertsalova <iryna.mertsalova@nerdysoft.com>
2024-11-20 12:32:57 +0000

    Deploying to prod from @ iryna-mertsalova/quiz-app@de50e4a9319f116bd771b4b169f14835ca9fb070 🚀

---
+++ chunk-V4IWH2I7.js
@@ -0,0 +1,2 @@
+import{J as m,L as s,M as r,f as a,l as t,m as n,t as i,v as p}from"./chunk-4NDLS4L7.js";var u=class o{static \u0275fac=function(e){return new(e||o)};static \u0275cmp=a({type:o,selectors:[["app-quiz"]],standalone:!0,features:[p],decls:5,vars:0,consts:[[1,"text-bright"],["link","/main"],["size","sm","type","ghost","iconState","prev"]],template:function(e,l){e&1&&(t(0,"p",0),i(1,` Quiz mocked page
+`),n(),t(2,"app-ui-link",1)(3,"app-ui-button",2),i(4,"Go to main page"),n()())},dependencies:[m,s,r],encapsulation:2})};export{u as QuizComponent};
