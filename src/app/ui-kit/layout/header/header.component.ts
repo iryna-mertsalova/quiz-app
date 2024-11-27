61a43da94e188b834c9cae7e23f60e28402e793c
iryna-mertsalova <iryna.mertsalova@nerdysoft.com>
2024-11-27 19:26:12 +0200

    fix: update the header for auto-closing

--- src/app/ui-kit/layout/header/header.component.ts
+++ src/app/ui-kit/layout/header/header.component.ts
@@ -1,21 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-ui-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
   logoSrc: string = 'main-logo.svg';
   isMenuOpen = false;
-
+  
   menuItems = [
     { link: 'quizzes-catalog', label: 'Quizzes catalog' },
     { link: 'quiz', label: 'Quiz' },
     { link: 'statistics', label: 'Statistics' },
   ];
 
+  private routerSubscription!: Subscription;
+
+  constructor(private router: Router) {}
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  ngOnInit(): void {
+    this.routerSubscription = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isMenuOpen = false;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
   }
 }
