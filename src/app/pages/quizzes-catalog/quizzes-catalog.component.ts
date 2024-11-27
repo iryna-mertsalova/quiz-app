7246410f8ade24c21d29c65fa5e7ae019f8a957b
iryna-mertsalova <iryna.mertsalova@nerdysoft.com>
2024-11-27 19:07:31 +0200

    fix: add subscription for quizzes catalog

--- src/app/pages/quizzes-catalog/quizzes-catalog.component.ts
+++ src/app/pages/quizzes-catalog/quizzes-catalog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UIKitModule } from '../../ui-kit/ui-kit.module';
 import { CommonModule } from '@angular/common';
 import {
@@ -7,7 +7,7 @@ import {
   cardItemStyles,
 } from '../../ui-kit/constants/card-item';
 import { CategoryModel } from '../../services/model/category.model';
-import { Observable } from 'rxjs';
+import { Observable, Subscription, take } from 'rxjs';
 import { StoreService } from '../../services/store.service';
 
 @Component({
@@ -16,21 +16,35 @@ import { StoreService } from '../../services/store.service';
   templateUrl: './quizzes-catalog.component.html',
   imports: [ UIKitModule, CommonModule ],
 })
-export class QuizzesCatalogComponent implements OnInit {
+export class QuizzesCatalogComponent implements OnInit, OnDestroy {
   categories$!: Observable<CategoryModel[]>;
   isLoading$!: Observable<boolean>;
+  subscription!: Subscription;
   
   constructor(private storeService: StoreService) { }
-
+  
   ngOnInit(): void {
+    this.subscription = this.storeService.getCategories()
+    .pipe(take(1))
+    .subscribe((categories) => {
+      if (!categories || categories.length === 0) {
+        this.storeService.loadCategories();
+      }
+    });
+    
     this.categories$ = this.storeService.getCategories();
     this.isLoading$ = this.storeService.getLoading();
-    this.storeService.loadCategories();
   }
-
+  
   getCardStyle(index: number): CardItemStyle {
     const styleIndex = index + Math.floor(index / 5);
     const enumValues = Object.values(CardColors);
     return cardItemStyles[enumValues[styleIndex % enumValues.length]];
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
