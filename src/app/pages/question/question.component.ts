c52004272f677a2ad793576bbaf8abcf2eb3438f
iryna-mertsalova <iryna.mertsalova@nerdysoft.com>
2024-12-03 18:52:53 +0200

    feat: add func for switching questions with mocked data

--- src/app/pages/question/question.component.ts
+++ src/app/pages/question/question.component.ts
@@ -1,17 +1,34 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { UIKitModule } from '../../ui-kit/ui-kit.module';
 import { ActivatedRoute } from '@angular/router';
+import { QUIZ_DATA } from '../../test-data/quiz';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-quiz',
   templateUrl: './question.component.html',
-  imports: [UIKitModule],
+  imports: [ UIKitModule, CommonModule ],
 })
 export class QuizComponent implements OnInit {
   @Input() quiz: string = '';
-  constructor(private route: ActivatedRoute) { }
+  quiz_data = QUIZ_DATA;
+  currentIndex: number = 0;
   
+  constructor(private route: ActivatedRoute) {}
+
+  nextQuestion() {
+    if (this.currentIndex < this.quiz_data.length - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  prevQuestion() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
   ngOnInit(): void {
     // eslint-disable-next-line dot-notation
     this.quiz = this.route.snapshot.params['name'];
--- src/app/ui-kit/components/question-item/question-item.component.ts
+++ src/app/ui-kit/components/question-item/question-item.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularSvgIconModule } from 'angular-svg-icon';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { QuestionModel } from '../../../services/model/question.model';
 
 @Component({
   selector: 'app-ui-question-item',
   templateUrl: './question-item.component.html',
 })
 export class QuestionItemComponent {
+  @Input() item: QuestionModel = { id: 0, question: '', answers: [] };
+  
+  @Output() next = new EventEmitter<void>(); 
+  @Output() prev = new EventEmitter<void>(); 
+
   images: string[] = [
     'avatars/Profile-1.svg',
     'avatars/Profile-2.svg',
     'avatars/Profile.svg',
-  ];
+  ];  
+
+  getFormattedText(): { start: string, middle: string, end: string } {
+    const isShortQuestion = this.item.question.length <= 20;
+    const middleStart = this.item.question.indexOf(' ', this.item.question.indexOf(' ') + 2);
+    const middleEnd = this.item.question.lastIndexOf(isShortQuestion ? '?' : ' ');
+
+    return {
+      start: this.item.question.slice(0, middleStart),
+      middle: this.item.question.slice(middleStart, middleEnd),
+      end: this.item.question.slice(middleEnd),
+    };
+  }
 }
