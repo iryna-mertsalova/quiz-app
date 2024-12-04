27325f97e6865d18f6b4f51d25d4e5cd452b2709
iryna-mertsalova <iryna.mertsalova@nerdysoft.com>
2024-12-04 18:03:56 +0200

    feat: continue work with switching functionality

--- src/app/ui-kit/components/question-item/question-item.component.ts
+++ src/app/ui-kit/components/question-item/question-item.component.ts
@@ -1,21 +1,39 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { QuestionModel } from '../../../services/model/question.model';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-ui-question-item',
   templateUrl: './question-item.component.html',
 })
-export class QuestionItemComponent {
+export class QuestionItemComponent {  
   @Input() item: QuestionModel = { id: 0, question: '', answers: [] };
-  
   @Output() next = new EventEmitter<void>(); 
   @Output() prev = new EventEmitter<void>(); 
-
+  
   images: string[] = [
     'avatars/Profile-1.svg',
     'avatars/Profile-2.svg',
     'avatars/Profile.svg',
   ];  
+  
+  form = new FormGroup({
+    selectedOption: new FormControl<string | null>(null),
+  });
+
+  get selectedAnswer() {
+    return this.form.get('selectedOption')?.value;
+  }
+
+  handlePrev(): void {
+    this.prev.emit();
+    this.form.reset();
+  }
+  
+  handleNext(): void {
+    this.next.emit();
+    this.form.reset();
+  }
 
   getFormattedText(): { start: string, middle: string, end: string } {
     const isShortQuestion = this.item.question.length <= 20;
