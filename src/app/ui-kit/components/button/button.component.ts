65b0bc17314f6e75edc96c1e96c3f45f7ce69eff
iryna-mertsalova <iryna.mertsalova@nerdysoft.com>
2025-01-02 13:58:51 +0200

    refactor: update svg-Style to use getter in the button

--- src/app/ui-kit/components/button/button.component.ts
+++ src/app/ui-kit/components/button/button.component.ts
@@ -5,63 +5,59 @@ import { TextTypes } from '../../constants/text-types';
   selector: 'app-ui-button',
   templateUrl: './button.component.html',
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
   @Input() type: 'accent' | 'ghost' = 'accent';
   @Input() size: 'sm' | 'md' = 'sm';
-  @Input() iconState: 'next' | 'prev' | 'none' = 'none';
   @Input() disabled: boolean = false;
   @Input() textType: TextTypes = 'P1';
 
-  imageSrc: string = 'chevron.svg';
-  iconRotate: string = '';
+  @Input()
+  set iconState(value: 'next' | 'prev' | 'none') {
+    this.iconPosition = value;
+    this.updateIconStyles();
+  }
+  
+  iconPosition: 'next' | 'prev' | 'none' = 'none';
   iconStyles: { [klass: string]: string | number } = {};
+  imageSrc: string = 'chevron.svg';
+  
+  private readonly iconStateClassesMap = {
+    next: { class: 'pr-3', rotate: '180deg' },
+    prev: { class: 'flex-row-reverse pl-3', rotate: '' },
+    none: { class: '', rotate: '' },
+  };
+  
+  private readonly sizeClassesMap = {
+    sm: 'py-1.5',
+    md: 'py-3',
+  };
+  
+  private readonly typeClassesMap = {
+    accent: 'bg-accent enabled:hover:bg-accent disabled:bg-secondary border-transparent',
+    ghost: 'border-accent enabled:hover:border-accent disabled:border-secondary',
+  };
 
+  get iconStateClasses(): string {
+    return this.iconStateClassesMap[this.iconPosition].class;
+  }
+  
+  get sizeClasses(): string {
+    return this.sizeClassesMap[this.size];
+  }
+  
+  get typeClasses(): string {
+    return this.typeClassesMap[this.type];
+  }
+  
   get classes(): string {
-    return [ this.iconStateStyle, this.sizeStyle, this.typeStyle ].join(' ');
+    return [ this.iconStateClasses, this.sizeClasses, this.typeClasses ].join(' ');
   }
 
-  get iconStateStyle(): string {
-    switch (this.iconState) {
-      case 'prev':
-        this.iconRotate = '0deg';
-        return 'flex-row-reverse pl-3';
-      case 'next':
-        this.iconRotate = '180deg';
-        return 'pr-3';
-      default:
-        return '';
-    }
-  }
-
-  get sizeStyle(): string {
-    if (this.size == 'sm') {
-      return 'py-1.5';
-    }
-    return 'py-3';
-  }
-
-  get typeStyle(): string {
-    switch (this.type) {
-      case 'accent':
-        return 'bg-accent enabled:hover:bg-accent disabled:bg-secondary border-transparent';
-      case 'ghost':
-        return 'border-accent enabled:hover:border-accent disabled:border-secondary';
-      default:
-        return '';
-    }
-  }
-
-  ngOnInit(): void {
-    if (this.iconState === 'next') {
-      this.iconRotate = '180deg';
-    }
-    if (this.iconState === 'prev') {
-      this.iconRotate = '0deg';
-    }
+  private updateIconStyles(): void {
     this.iconStyles = {
       'width.px': 14,
       'height.px': 14,
-      'transform': `rotate(${this.iconRotate})`,
+      'transform': `rotate(${this.iconStateClassesMap[this.iconPosition].rotate})`,
     };
   }
 }
