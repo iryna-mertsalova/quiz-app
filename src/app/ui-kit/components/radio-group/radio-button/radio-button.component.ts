7370f6e225c0eeef8fd0a55bb2d2905ba5d4a949
iryna-mertsalova <iryna.mertsalova@nerdysoft.com>
2025-01-02 12:10:23 +0200

    refactor: update html file using tailwindcss classes and plugins and remove scss file from radio-button

--- src/app/ui-kit/components/radio-group/radio-button/radio-button.component.ts
+++ src/app/ui-kit/components/radio-group/radio-button/radio-button.component.ts
@@ -4,7 +4,6 @@ import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-ui-radio-button',
   templateUrl: './radio-button.component.html',
-  styleUrls: ['./radio-button.component.scss'],
 })
 export class RadioButtonComponent {
   @Input() text: string = '';
